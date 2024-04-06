f799e5f22badab20405a10317df112f1fe0bd6b3
osm3rr <parabavireo@gmail.com>
2024-04-06 16:47:30 -0400

    login added

--- blogs/models.py
+++ blogs/models.py
@@ -6,6 +6,7 @@ class Publication(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         "auth.User",
+        
         on_delete=models.CASCADE,
     )
     body = models.TextField()
